c5f6740ef5ff0666df7a987a71e2bf9f3750c2ab
de-FauLtt <yashshuklash007@gmail.com>
2024-09-14 22:06:49 +0530

    Updated todolist files

--- dbconnect.js
+++ dbconnect.js
@@ -3,8 +3,8 @@ const client=new MongoClient("mongodb://localhost:27017/");
 async function getconnect()
 {
     let con=await client.connect();
-    let db=con.db("");
-    let collection=db.collection("");
+    let db=con.db("todolist");
+    let collection=db.collection("tasks");
     return collection;
 }
 module.exports=getconnect;
---
+++ index.js
@@ -0,0 +1,43 @@
+const express= require("express");
+const getconnect=require("./dbconnect");
+const app=express();
+app.set("view engine","ejs");
+app.get("/home",async (req,res)=>
+{
+    let collection=await getconnect();
+    let records=await collection.find({}).toArray();
+    res.render("home",{records});
+});
+app.get("/insert",(req,res)=>{
+    res.render("insert");
+});
+app.get("/insertres",async (req,res)=>
+{
+    let gtask=req.query.gtask;
+    let collection=await getconnect();
+    let r=await collection.insertOne({task:gtask});
+    let records=await collection.find({}).toArray();
+    res.render("show",{records});
+});
+app.get("/deletedata",async (req,res)=>{
+    let gtask=req.query.gtask;
+    collection=await getconnect();
+    let r=await collection.deleteOne({task:gtask});
+    let records=await collection.find({}).toArray();
+    res.render("show",{records});
+});
+app.get("/updatedata",async (req,res)=>{
+    let gtask=req.query.gtask;
+    let collection=await getconnect();
+    let records=await collection.find({task:gtask}).toArray();
+    res.render("update1",{records});
+});
+app.get("/updateres",async (req,res)=>
+    {
+        let gtask=req.query.gtask;
+        let collection=await getconnect();
+        let r=await collection.updateOne({task:gtask},{$set:{task:gtask}});
+        let records=await collection.find({}).toArray();
+        res.render("show",{records});
+    });
+app.listen(5000,()=>console.log("Server is running"));
