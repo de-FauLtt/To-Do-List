59f111f57b8652c252a47a1d4d47d07ea4cd81a7
de-FauLtt <yashshuklash007@gmail.com>
2024-09-14 22:16:04 +0530

    edited home page

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.get("/insertres",async (req,res)=>
     let collection=await getconnect();
     let r=await collection.insertOne({task:gtask});
     let records=await collection.find({}).toArray();
-    res.render("show",{records});
+    res.render("home",{records});
 });
 app.get("/deletedata",async (req,res)=>{
     let gtask=req.query.gtask;
     collection=await getconnect();
     let r=await collection.deleteOne({task:gtask});
     let records=await collection.find({}).toArray();
-    res.render("show",{records});
+    res.render("home",{records});
 });
 app.get("/updatedata",async (req,res)=>{
     let gtask=req.query.gtask;
@@ -38,6 +38,6 @@ app.get("/updateres",async (req,res)=>
         let collection=await getconnect();
         let r=await collection.updateOne({task:gtask},{$set:{task:gtask}});
         let records=await collection.find({}).toArray();
-        res.render("show",{records});
+        res.render("home",{records});
     });
 app.listen(5000,()=>console.log("Server is running"));
